c10e420abe58e86df3f08f8fbf1ec426a3e129c4
grisya <3505005893@qq.com>
2022-04-10 20:05:54 +0800

    上传season.js

--- packageB/pages/season/season.js
+++ packageB/pages/season/season.js
@@ -7,7 +7,121 @@ Page({
     data: {
 
     },
-
+    gotolichun(){
+        wx.navigateTo({
+          url: '/packageB_A/pages/lichun/lichun',
+        })
+    },
+    gotoyushui(){
+        wx.navigateTo({
+          url: '/packageB_A/pages/yushui/yushui',
+        })
+    },
+    gotojingzhe(){
+        wx.navigateTo({
+          url: '/packageB_A/pages/jingzhe/jingzhe',
+        })
+    },
+    gotoqingming(){
+        wx.navigateTo({
+          url: '/packageB_A/pages/qingming/qingming',
+        })
+    },
+    gotoguyu(){
+        wx.navigateTo({
+          url: '/packageB_A/pages/guyu/guyu',
+        })
+    },
+    gotolixia(){
+        wx.navigateTo({
+          url: '/packageB_A/pages/lixia/lixia',
+        })
+    },
+    gotoxiaoman(){
+        wx.navigateTo({
+          url: '/packageB_A/pages/xiaoman/xiaoman',
+        })
+    },
+    gotomangzhong(){
+        wx.navigateTo({
+          url: '/packageB_A/pages/mangzhong/mangzhong',
+        })
+    },
+    gotoxiazhi(){
+        wx.navigateTo({
+          url: '/packageB_A/pages/xiazhi/xiazhi',
+        })
+    },
+    gotoxiaoshu(){
+        wx.navigateTo({
+          url: '/packageB_A/pages/xiaoshu/xiaoshu',
+        })
+    },
+    gotodashu(){
+        wx.navigateTo({
+          url: '/packageB_A/pages/xiaoshu/xiaoshu',
+        })
+    },
+    gotoliqiu(){
+        wx.navigateTo({
+          url: '/packageB_A/pages/liqiu/liqiu',
+        })
+    },
+    gotochushu(){
+        wx.navigateTo({
+          url: '/packageB_A/pages/chushu/chushu',
+        })
+    },
+    gotobailu(){
+        wx.navigateTo({
+          url: '/packageB_A/pages/bailu/bailu',
+        })
+    },
+    gotoqiufen(){
+        wx.navigateTo({
+          url: '/packageB_A/pages/qiufen/qiufen',
+        })
+    },
+    gotohanlu(){
+        wx.navigateTo({
+          url: '/packageB_A/pages/hanlu/hanlu',
+        })
+    },
+    gotoshuangjiang(){
+        wx.navigateTo({
+          url: '/packageB_A/pages/shuangjiang/shuangjiang',
+        })
+    },
+    gotolidong(){
+        wx.navigateTo({
+          url: '/packageB_A/pages/lidong/lidong',
+        })
+    },
+    gotoxiaoxue(){
+        wx.navigateTo({
+          url: '/packageB_A/pages/xiaoxue/xiaoxue',
+        })
+    },
+    gotodaxue(){
+        wx.navigateTo({
+          url: '/packageB_A/pages/daxue/daxue',
+        })
+    },
+    gotodongzhi(){
+        wx.navigateTo({
+          url: '/packageB_A/pages/dongzhi/dongzhi',
+        })
+    },
+    gotoxiaohan(){
+        wx.navigateTo({
+          url: '/packageB_A/pages/xiaohan/xiaohan',
+        })
+    },
+    gotodahan(){
+        wx.navigateTo({
+          url: '/packageB_A/pages/dahan/dahan',
+        })
+    },
     /**
      * 生命周期函数--监听页面加载
      */
