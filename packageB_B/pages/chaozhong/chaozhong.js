89575b355d201e3577306b5e36b34ed4e986ef87
grisya <3505005893@qq.com>
2022-04-17 19:53:28 +0800

    上传

---
+++ packageB_B/pages/chaozhong/chaozhong.js
@@ -0,0 +1,120 @@
+// packageB_B/pages/chaozhong/chaozhong.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+
+    },
+    gotoqinghuayu(){
+        wx.navigateTo({
+          url: '/packageB_B_A/pages/qinghuayu/qinghuayu',
+        })
+    },
+    gotoyunijixiongrou(){
+        wx.navigateTo({
+          url: '/packageB_B_A/pages/yuni/yuni',
+        })
+    },
+    gotojixiongroupai(){
+        wx.navigateTo({
+          url: '/packageB_B_A/pages/jipai/jipai',
+        })
+    },
+    gotoliangbanjixiongrou(){
+        wx.navigateTo({
+          url: '/packageB_B_A/pages/liangban/liangban',
+        })
+    },
+    gotosuantai(){
+        wx.navigateTo({
+          url: '/packageB_B_A/pages/suantai/suantai',
+        })
+    },
+    gotokusun(){
+        wx.navigateTo({
+          url: '/packageB_B_A/pages/kusun/kusun',
+        })
+    },
+    gotoheihujiao(){
+        wx.navigateTo({
+          url: '/packageB_B_A/pages/heihujiao/heihujiao',
+        })
+    },
+    gotosanwenyu(){
+        wx.navigateTo({
+          url: '/packageB_B_A/pages/sanwenyu/sanwenyu',
+        })
+    },
+    gotoxueyu(){
+        wx.navigateTo({
+          url: '/packageB_B_A/pages/xueyu/xueyu',
+        })
+    },
+    gotoxilanhua(){
+        wx.navigateTo({
+          url: '/packageB_B_A/pages/xilanhua/xilanhua',
+        })
+    },
+    gotoyanshuiya(){
+        wx.navigateTo({
+          url: '/packageB_B_A/pages/yanshuiya/yanshuiya',
+        })
+    },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
---
+++ packageB_B/pages/feipang/feipang.js
@@ -0,0 +1,116 @@
+// packageB_B/pages/feipang/feipang.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+
+    },
+    gotohuanggua(){
+        wx.navigateTo({
+          url: '/packageB_B_B/pages/huanggua/huanggua',
+        })
+    },
+    gotohaidai(){
+        wx.navigateTo({
+          url: '/packageB_B_B/pages/haidai/haidai',
+        })
+    },
+    gotomachixian(){
+        wx.navigateTo({
+          url: '/packageB_B_B/pages/machixian/machixian',
+        })
+    },
+    gotokugua(){
+        wx.navigateTo({
+          url: '/packageB_B_B/pages/kugua/kugua',
+        })
+    },
+    gotohelandou(){
+        wx.navigateTo({
+          url: '/packageB_B_B/pages/helandou/helandou',
+        })
+    },
+    gotoshala(){
+        wx.navigateTo({
+          url: '/packageB_B_B/pages/shala/shala',
+        })
+    },
+    gotolimai(){
+        wx.navigateTo({
+          url: '/packageB_B_B/pages/limai/limai',
+        })
+    },
+    gotobingcai(){
+        wx.navigateTo({
+          url: '/packageB_B_B/pages/bingcai/bingcai',
+        })
+    },
+    gotoqincai(){
+        wx.navigateTo({
+          url: '/packageB_B_B/pages/qincai/qincai',
+        })
+    },
+    gotobaoyu(){
+        wx.navigateTo({
+          url: '/packageB_B_B/pages/baoyu/baoyu',
+        })
+    },
+   
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
